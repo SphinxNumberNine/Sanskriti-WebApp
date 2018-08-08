417d91cf98b5485852a02cb578ea8d87d44c5b3c
Ashwath Krishnan <ashwathkrishnan@yahoo.com>
2018-08-08 15:47:59 -0400

    Improvements to class edit page

--- Server/client/src/components/admin/ClassEdit.js
+++ Server/client/src/components/admin/ClassEdit.js
@@ -2,24 +2,122 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class ClassEdit extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.danceClass) {
+      var danceClass = nextProps.danceClass.danceClass;
+      this.setState(danceClass);
+    }
+  }
+
+  renderDayButton(button, checked) {
+    var firstLetter = button.charAt(0);
+    const labelText = firstLetter.toUpperCase() + button.substring(1);
+    return (
+      <p>
+        <input
+          className="with-gap"
+          name="group1"
+          type="radio"
+          id={labelText}
+          checked={checked}
+          onChange={this.handleDayOfWeekChange.bind(this)}
+        />
+        <label htmlFor={labelText}>{labelText}</label>
+      </p>
+    );
+  }
+
+  renderDaysOfWeekButtons() {
+    const days = [
+      "sunday",
+      "monday",
+      "tuesday",
+      "wednesday",
+      "thursday",
+      "friday",
+      "saturday"
+    ];
+    var buttons = [];
+    for (var x = 0; x < days.length; x++) {
+      console.log(days[x] + " - " + this.state.dayOfWeek);
+      if (days[x].toUpperCase() === this.state.dayOfWeek.toUpperCase()) {
+        buttons.push(this.renderDayButton(days[x], true));
+      } else {
+        buttons.push(this.renderDayButton(days[x], false));
+      }
+    }
+
+    return buttons;
+  }
+
   renderForm() {
     if (this.props.danceClass) {
       return (
-        <form>
-          <h6>Class Name:</h6>
-          <input
-            type="text"
-            name="classname"
-            id="classname_id"
-            placeholder={this.props.danceClass.name}
-          />
-        </form>
+        <div>
+          <div className="row">
+            <div className="input-field col s6">
+              <input
+                value={this.state.name || ""}
+                id="first_name"
+                type="text"
+                className="validate"
+                onChange={this.handleNameChange.bind(this)}
+              />
+              <label className="active" htmlFor="first_name">
+                Name:
+              </label>
+            </div>
+            <div className="input-field col s6">
+              <input
+                value={this.formatFeeString(this.state.fee) || ""}
+                id="first_name"
+                type="number"
+                className="validate"
+                onChange={this.handleFeeChange.bind(this)}
+              />
+              <label className="active" htmlFor="first_name">
+                Fee:
+              </label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s3">
+              <label>Day of Week:</label>
+              {this.renderDaysOfWeekButtons()}
+            </div>
+          </div>
+        </div>
       );
     }
   }
 
   render() {
-    return <div>{this.renderForm()}</div>;
+    return (
+    <div>
+      <div style={{ textAlign: "center" }}>
+        <h5>Class Edit</h5>
+      </div>  
+      {this.renderForm()}
+    </div>
+    )
+  }
+
+  handleNameChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  handleFeeChange(e) {
+    this.setState({ fee: "$" + e.target.value });
+  }
+
+  handleDayOfWeekChange(e) {
+    if(e.target.checked) {
+      this.setState({ dayOfWeek: e.target.id });
+    }
+  }
+
+  formatFeeString(feeString) {
+    return feeString.replace("$", "");
   }
 }
 
